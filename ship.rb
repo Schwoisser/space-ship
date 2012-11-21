506fc1be821870ed96eb1a85bb47bfe2cb256d31
me <spamleech@fantasymail.de>
2012-11-21 20:15:21 +0100

    trade

--- ship.rb
+++ ship.rb
@@ -40,7 +40,17 @@ class Ship
   end
   
   def unload(good)
-    
+    goods.each do |g|
+      if g.name == good.name && g.amount >= good.amount
+        g.amount -= good.amount
+        
+        if g.amount == 0
+          goods.delete g
+        end
+        return true
+      end
+    end
+    return false
   end
   
   def free_capacity
@@ -199,8 +209,7 @@ class Ship
     end
   end
   
-  def 
-  
+
   def weopon(command)
   end
 
--- space/planetary/planet.rb
+++ space/planetary/planet.rb
@@ -8,6 +8,7 @@ class Planet
     @ships = []
     @goods = []
     @government_attributes = []
+    @production=[]
   end
   
   def tick
@@ -23,11 +24,27 @@ class Planet
   end
   
   def buy(good)
-    @goods.
+    goods.each do |g|
+      if g.name == good.name && g.amount >= good.amount
+        g.amount -= good.amount
+        
+        if g.amount == 0
+          goods.delete g
+        end
+        return true
+      end
+    end
+    return false
   end
   
   def sell(good)
-    
+        if free_capacity >= good.amount
+      goods << good
+      true
+    else
+      false
+    end
   end
+
   
 end
